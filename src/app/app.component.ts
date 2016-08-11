7b8c83195a616ddc2b4d3f96414403f8067305c4
Jan Valentik <jvalentik@sk.ibm.com>
2016-08-11 23:42:26 +0200

    ngContent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,13 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'first-app',
-    template: `
-            <h1>Inline Template</h1>
-            <p>in two lines</p>
-            <app-other></app-other>
-            <app-another></app-another>
-    `,
+    templateUrl: 'app.component.html',
     styles: [`
-    h1 {
-      color: red;
-    }
+      h1 {
+        color: red;
+      }
     `]
   }
 )
-export class AppComponent {}
+export class AppComponent {
+}
