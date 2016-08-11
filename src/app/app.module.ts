490fd8a2b41146e587e8be493cf77798f3e27d6a
Jan Valentik <jvalentik@sk.ibm.com>
2016-08-11 23:57:38 +0200

    string interploation data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { OtherComponent, AnotherComponent } from './other';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OtherComponent,
-    AnotherComponent
+    AnotherComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule
